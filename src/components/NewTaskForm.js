4f0eac5a7c83d42353bae0922aa20c307d05cb06
ryanrizeq <125945925+ryanrizeq@users.noreply.github.com>
2023-06-09 23:41:58 -0700

    Update NewTaskForm.js

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const categoriesToPick = categories.filter((category) => category !== "All")
+  const [text, setText] = useState('')
+  const [category, setCategory] = useState('Code')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    onTaskFormSubmit({ text, category })
+    setText('')
+    setCategory('Code')
+  }
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={text} onChange={(e)=>{setText(e.target.value)}} />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" onChange={(e)=>{setCategory(e.target.value)}}>
+          {categoriesToPick.map((category) => {
+            return <option key={category}>{category}</option>
+          })}
         </select>
       </label>
       <input type="submit" value="Add task" />
